17ffdfe0537245fb50ef504b968f78bde21fde1d
Nurbek Malikov <nurbek_malikov@epam.com>
2025-05-10 01:12:01 +0500

    Initial setup

---
+++ core/src/test/java/com/malikov/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malikov.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
